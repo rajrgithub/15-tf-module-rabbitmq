9d8cbd9bfbe921d9991dbde879c29c8b241f1dc3
brajasekharreddy <raj479.mits@gmail.com>
2023-02-28 02:02:41 +0530

    ec2 instance for rabbitmq - IAM profile to get aws parameters since we are not storing aws credentials

--- main.tf
+++ main.tf
@@ -1,3 +1,71 @@
+// Create AWS IAM roles to avoid configuring AWS credentials to get AWS Paramaters
+resource "aws_iam_role" "role" {
+  name = "${var.env}-${var.component}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-${var.component}-role" }
+  )
+}
+
+//create instance profile for the role
+resource "aws_iam_instance_profile" "profile" {
+  name = "${var.env}-${var.component}-role"
+  role = aws_iam_role.role.name
+}
+
+// Create policy to access the AWS Paramaters using the IAM role
+resource "aws_iam_policy" "policy" {
+  name        = "${var.env}-${var.component}-parameter-store-policy"
+  path        = "/"
+  description = "${var.env}-${var.component}-parameter-store-policy"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "ssm:GetParameterHistory",
+          "ssm:GetParametersByPath",
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        "Resource" : [
+          "arn:aws:ssm:us-east-1:973130779128:parameter/${var.env}.${var.component}*"
+        ]
+      },
+      {
+        "Sid" : "VisualEditor1",
+        "Effect" : "Allow",
+        "Action" : "ssm:DescribeParameters",
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+// Attach the policy to the role
+resource "aws_iam_role_policy_attachment" "role-attach" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
 resource "aws_security_group" "rabbitmq" {
   name        = "${var.env}-rabbitmq-security-group"
   description = "${var.env}-rabbitmq-security-group"
@@ -76,7 +144,7 @@ resource "aws_spot_instance_request" "rabbitmq" {
   vpc_security_group_ids = [aws_security_group.rabbitmq.id]
   wait_for_fulfillment   = true
   user_data              = base64encode(templatefile("${path.module}/user-data.sh", { component = "rabbitmq", env = var.env }))
-  //iam_instance_profile   = aws_iam_instance_profile.profile.name
+  iam_instance_profile   = aws_iam_instance_profile.profile.name
 
   tags = merge(
     local.common_tags,

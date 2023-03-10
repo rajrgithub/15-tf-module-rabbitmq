8783f1bd5752e6ef14a0bdb5a1edc882b95d09f9
brajasekharreddy <raj479.mits@gmail.com>
2023-03-11 02:40:07 +0530

    PROD - Changes for prod instances

--- vars.tf
+++ vars.tf
@@ -2,10 +2,11 @@ variable "env" {}
 variable "subnet_ids" {}
 variable "vpc_id" {}
 variable "allow_cidr" {}
-variable "engine_type" {}
-variable "engine_version" {}
-variable "host_instance_type" {}
-variable "deployment_mode" {}
+# NOT Required
+#variable "engine_type" {}
+#variable "engine_version" {}
+#variable "host_instance_type" {}
+#variable "deployment_mode" {}
 variable "bastion_cidr" {}
 variable "component" {
   default = "rabbitmq"
